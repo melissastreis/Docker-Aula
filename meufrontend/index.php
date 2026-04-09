0d39a3fd765030c3ed9e528b6248e306e0ecd6b0
melissastreis <melissatakeda23@gmail.com>
2026-04-09 20:15:45 -0300

    Change language and update title in index.php Atualizando codigo

--- meufrontend/index.php
+++ meufrontend/index.php
@@ -1,11 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>GPP Guaianases</title>
+  <link rel="manifest" href="manifest.json">
+  <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    <h1>Meu nome é Melissa</h1>
+  <h1>Hello Word</h1>
+  
+  <?php 
+  $nome ="Melissa";
+
+  echo "Olá $nome, boa aula!";
+  ?>
 </body>
+
 </html>
